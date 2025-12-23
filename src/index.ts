92172437d8d1fc4c85dcae7a7e72066d4be36dad
Philippe Skopal <skopal.philippe@gmail.com>
2025-12-23 17:20:26 +0100

    Solution as comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-// Start coding here!
+// Parse Database
 
-console.log("Hello, TypeScriptttt!");
+// Parse SQL Request into an object through a builder 
 
+
+// Sending the SQL Request object to the database
+
+// The datase create an instance of a class for a result lets call it : "intermediary"
+    // intermediary will have a clone of the main table inside, with all unfiltered rows
+        // FROM
+    // JOIN -> Will have a list of tables instance, will all rows
+    // WHERE: 
+        // Flatten all the rows, will result on duplicated data
+        // Filter all the rows that does not match the condition
+        // SELECT -> Filter all unwanted columns
